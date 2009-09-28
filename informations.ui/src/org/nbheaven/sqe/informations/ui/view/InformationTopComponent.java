7144568635d0abf78c25e8821d698d7f7466d2d9
Jesse Glick <jglick@cloudbees.com>
2009-09-28 13:35:59 -0400

    Possible fix for #167811: NPE.

--- informations.ui/src/org/nbheaven/sqe/informations/ui/view/InformationTopComponent.java
+++ informations.ui/src/org/nbheaven/sqe/informations/ui/view/InformationTopComponent.java
@@ -79,7 +79,9 @@ public class InformationTopComponent extends TopComponent implements PropertyCha
         label.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
         this.add(label, BorderLayout.NORTH);
         SQEInformationProvider provider = project.getLookup().lookup(SQEInformationProvider.class);
-        this.add(provider.getInformationComponent().getComponent(), BorderLayout.CENTER);
+        if (provider != null) { // #167811?
+            this.add(provider.getInformationComponent().getComponent(), BorderLayout.CENTER);
+        }
     }
 
     @Override
