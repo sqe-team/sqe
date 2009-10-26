349138ede805c86194944b772305bba2f0d8451c
Jesse Glick <jglick@cloudbees.com>
2009-10-26 12:51:01 -0400

    Fixing copyright.

--- tools.pmd.codedefects.projects.maven/src/org/nbheaven/sqe/tools/pmd/codedefects/projects/maven/MavenDefaults.java
+++ tools.pmd.codedefects.projects.maven/src/org/nbheaven/sqe/tools/pmd/codedefects/projects/maven/MavenDefaults.java
@@ -1,6 +1,19 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+/* Copyright 2009 Milos Kleint
+ *
+ * This file is part of the Software Quality Environment Project.
+ *
+ * The Software Quality Environment Project is free software:
+ * you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation,
+ * either version 2 of the License, or (at your option) any later version.
+ *
+ * The Software Quality Environment Project is distributed in the hope that
+ * it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 package org.nbheaven.sqe.tools.pmd.codedefects.projects.maven;
